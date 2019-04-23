e6c3902bd5369d2420d0d427db329d675195bf2d
unknown <capitalex@protonmail.com>
2019-04-23 00:57:56 -0400

    Changed Column Pane to simpler implementation
    While slowwer, it is easy to design and implement

--- src/org/openjfx/note/ColumnPane.java
+++ src/org/openjfx/note/ColumnPane.java
@@ -1,129 +1,85 @@
 package org.openjfx.note;
 
-import javafx.geometry.HPos;
-import javafx.geometry.VPos;
+import javafx.geometry.Pos;
 import javafx.scene.Node;
-import javafx.scene.layout.Pane;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
+public class NoteLayout extends FlowPane {
+    private ArrayList<Node> nodes = new ArrayList<>();
+    private int columnCount = -1;
+    private int oldColumnCount = -2;
+    private double columnWidth = 125;
+    private double hgap;
 
-public class ColumnPane extends Pane {
-    private Column[] columns;
-    private double columnWidth = 150;
-    private double vgap = 0;
-    private double hgap = 10;
-    int columnCount = -1;
-
-
-    public ColumnPane() {
-        super();
-        columnWidth = 150;
+    public NoteLayout() {
+        widthProperty().addListener((observable, oldValue, newValue) -> {
+            updateColumns(false);
+        });
+        setAlignment(Pos.TOP_CENTER);
+        setHgap(5);
     }
 
-    public ColumnPane(Node... children) {
-        super();
-        getChildren().addAll(children);
-    }
-
-    @Override
-    protected double computeMinHeight(double width) {
-        return super.computePrefHeight(width);
-    }
-
-    @Override
-    protected double computePrefHeight(double width) {
-        if (columns == null)
-            return 0;
-        else
-            return getLongestColumn(columns).height;
-    }
-
-    private static int columnCount(double width, double columnWidth) {
-        int maxColumns = (int) (width / columnWidth);
-        return maxColumns > 0 ? maxColumns : maxColumns + 1;
-    }
-
-    private static Column getLongestColumn(Column[] list) {
-        int longest = 0;
-        for (int i = 0; i < list.length; i++) {
-            if (list[i].height > list[longest].height) {
-                longest = i;
-            }
-        }
-        return list[longest];
-    }
-
-    private static Column getShortestColumn(Column[] list) {
+    private VBox shortestRow() {
         int shortest = 0;
-        for (int i = 0; i < list.length; i++) {
-            if (list[i].height < list[shortest].height) {
+        var children = getChildren();
+
+        for (int i = 0; i < children.size(); i++) {
+            var a = children.get(i);
+            var b = children.get(shortest);
+            double aheight = a.prefHeight(columnWidth);
+            double bheight = b.prefHeight(columnWidth);
+            if (aheight < bheight) {
                 shortest = i;
             }
+
         }
-        return list[shortest];
+        return (VBox) children.get(shortest);
     }
 
-    private static Column[] getEmptyColumns(double width, double columnWidth) {
-        int columnCount = columnCount(width, columnWidth);
-        Column[] columns = new Column[columnCount];
-        for (int i = 0; i < columnCount; i++) {
-            columns[i] = new Column();
+    private static int calculateColumns(double columnWidth, double innerWidth, double hgap) {
+        int columns = (int) (innerWidth / columnWidth);
+        // Recalculate to account for HGap
+        if (columns * (columnWidth + hgap) > innerWidth) {
+            return columns - 1 > 0 ? columns - 1 : 1;
+        } else {
+            return columns > 0 ? columns : 1;
         }
-
-        return columns;
     }
 
-    private static Column[] getColumns(List<Node> nodes, double width, double columnWidth) {
-        Column[] columns = getEmptyColumns(width, columnWidth);
-        for (int i = nodes.size() - 1; i >= 0; i--) {
-            var node = nodes.get(i);
-            var column = getShortestColumn(columns);
-            var rect = new Rect();
-            rect.height = node.prefHeight(columnWidth);
-            rect.node = node;
-            rect.y = column.height;
-            column.rects.add(rect);
-            column.height += rect.height;
-        }
-        return columns;
+    private static VBox vbox(double columnWidth) {
+        VBox vbox = new VBox();
+        vbox.setMaxWidth(columnWidth);
+        vbox.setMinWidth(columnWidth);
+        return vbox;
     }
 
-
-    // Switch to column based system that choices the
-    // shortest column to move a child node to
-    @Override
-    protected void layoutChildren() {
+    private void updateColumns(boolean force){
         double innerWidth = getWidth() - getInsets().getLeft() - getInsets().getRight();
-        double leftPadding = getInsets().getLeft();
-        double topPadding = getInsets().getTop();
-        columns = getColumns(getChildren(), innerWidth, columnWidth);
-        for (int i = 0; i < columns.length; i++) {
-            for (int j = 0; j < columns[i].rects.size(); j++) {
-                var rect = columns[i].rects.get(j);
-                layoutInArea(
-                        rect.node,
-                        i * columnWidth + leftPadding + (j != 0 ? hgap : 0),
-                        rect.y + topPadding,
-                        columnWidth,
-                        rect.height,
-                        0,
-                        HPos.LEFT,
-                        VPos.TOP
-                );
+
+        columnCount = calculateColumns(columnWidth, innerWidth, getHgap());
+        if (columnCount != oldColumnCount || force) {
+            var children = getChildren();
+            children.clear();
+            for (int i = 0; i < columnCount; i++)
+                children.add(vbox(columnWidth));
+
+            for (int i = nodes.size(); i > 0; i--) {
+                var node = nodes.get(i - 1);
+                VBox shortest = shortestRow();
+                shortest.getChildren().add(node);
             }
         }
+        oldColumnCount = columnCount;
     }
 
-    static private class Rect {
-        double height, y;
-        Node node;
-
-    }
-
-    static private class Column {
-        double height;
-        ArrayList<Rect> rects = new ArrayList<>();
+    public void addNode(Node view) {
+        nodes.add(view);
+        updateColumns(true);
     }
 }
